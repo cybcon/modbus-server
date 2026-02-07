7115b53e55d038829b7a0e5cca1accc76d165d28
Michael Oberdorf <info@oberdorf-itc.de>
2026-02-07 14:31:16 +0100

    enhance python library path

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+__application_lib_path__ = os.path.join(os.path.dirname(os.path.dirname(__file__)), "src", "app")
+sys.path.append(__application_lib_path__)
